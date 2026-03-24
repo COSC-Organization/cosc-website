4056e9b821d0303326f3fa35f2e6f51a6b1c0626
Kaushik H S <kaushik0h0s@gmail.com>
2026-03-24 22:01:09 +0530

    Add react-hooks plugin to ESLint configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import js from "@eslint/js";
 import tsParser from "@typescript-eslint/parser";
 import tsPlugin from "@typescript-eslint/eslint-plugin";
+import reactHooks from "eslint-plugin-react-hooks";
 
 export default [
   {
